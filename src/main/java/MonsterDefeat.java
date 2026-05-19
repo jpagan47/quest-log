6bed218d79d1eb3a67a3a6c8234d7d0ad66b2baa
josepagan <josepaganrnj@outlook.com>
2026-05-19 10:14:32 -0400

    Filled out my method logic for m MonsterDefeat class

--- src/main/java/MonsterDefeat.java
+++ src/main/java/MonsterDefeat.java
@@ -1,2 +1,54 @@
 public class MonsterDefeat {
+    private String monsterName;
+    private int difficultyLevel;
+    private boolean boss;
+
+
+    public String getName(){
+        return this.monsterName;
+    }
+    public int getPoints(){
+       int point  = this.difficultyLevel * 50;
+       if (boss){
+           point += 100;
+       }
+       return point;
+    }
+    public String getSummary(){
+        return "Defeated " + this.monsterName + "for " + getPoints() + " points";
+    }
+
+
+
+
+
+
+
+    public String getMonsterName() {
+        return monsterName;
+    }
+
+    public void setMonsterName(String monsterName) {
+        this.monsterName = monsterName;
+    }
+
+    public int getDifficultyLevel() {
+        return difficultyLevel;
+    }
+
+    public void setDifficultyLevel(int difficultyLevel) {
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    public boolean isBoss() {
+        return boss;
+    }
+
+    public void setBoss(boolean boss) {
+        this.boss = boss;
+    }
+
+
+
+
 }
