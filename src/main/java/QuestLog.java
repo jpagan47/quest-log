c4368257ac5cc64a130c353b07f6a0b1ee26782a
josepagan <josepaganrnj@outlook.com>
2026-05-19 11:23:14 -0400

    QuestLog class method logic filled out.

--- src/main/java/QuestLog.java
+++ src/main/java/QuestLog.java
@@ -1,5 +1,58 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class QuestLog {
     private List<ScoreEvent> events;
+
+    public QuestLog() {
+        events = new ArrayList<>();
+    }
+
+    public void addEvent(ScoreEvent event) {
+        events.add(event);
+    }
+
+    public int getTotalScore() {
+        int totalPoints = 0;
+        for (ScoreEvent sE : events) {
+            totalPoints += sE.getPoints();
+        }
+        return totalPoints;
+    }
+
+    public ScoreEvent getHighestScoringEvent() {
+        int highestPoint = 0;
+        ScoreEvent scoreEvent = null;
+        for (ScoreEvent score : events) {
+            if (score.getPoints() > highestPoint) {
+                highestPoint = score.getPoints();
+                scoreEvent = score;
+            }
+        }
+        return scoreEvent;
+
+    }
+
+    public ScoreEvent getLowestScoringEvent() {
+        int lowestPoint = events.get(0).getPoints();
+        ScoreEvent scoreEvent = null;
+        for (ScoreEvent score : events) {
+            if (score.getPoints() < lowestPoint) {
+                lowestPoint = score.getPoints();
+                scoreEvent = score;
+            }
+        }
+        return scoreEvent;
+    }
+
+    public void printSummary() {
+        for (ScoreEvent sE : events) {
+            System.out.println(sE.getSummary());
+
+        }
+        System.out.println("Total Score is:" + getTotalScore());
+        for (ScoreEvent sE : events) {
+            System.out.println(sE.getPoints());
+        }
+    }
 }
