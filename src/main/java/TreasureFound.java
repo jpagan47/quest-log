140de851aad871179aa0a9895e9508f2f7942554
josepagan <josepaganrnj@outlook.com>
2026-05-19 10:28:06 -0400

    Filled out my method logic in the TreasureFound class

--- src/main/java/TreasureFound.java
+++ src/main/java/TreasureFound.java
@@ -1,2 +1,48 @@
 public class TreasureFound {
+    private String treasureName;
+    private String rarity;
+
+    public TreasureFound(String treasureName, String rarity) {
+        this.treasureName = treasureName;
+        this.rarity = rarity;
+    }
+
+    public void setTreasureName(String treasureName) {
+        this.treasureName = treasureName;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+
+
+
+    public String getName() {
+        return this.treasureName;
+    }
+
+    public int getPoints() {
+        int points = 0;
+        if (this.rarity.equalsIgnoreCase("common")) {
+            points = 25;
+        }
+        else if (this.rarity.equalsIgnoreCase("rare")) {
+            points = 75;
+        }
+        else if (this.rarity.equalsIgnoreCase("legendary")) {
+            points = 150;
+        }
+        else {
+            points = 0;
+        }
+        return points;
+    }
+
+    public String getSummary(){
+        return "Found " + getRarity() + " " + getName() +" " +getPoints();
+    }
 }
